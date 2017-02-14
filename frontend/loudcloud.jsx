48589c97ae9c16e590bc8ac6abc6971f009eb417
Tassosb <tassosbareiss@gmail.com>
2017-02-14 12:51:51 -0500

    Setup frontend skeleton

---
+++ frontend/loudcloud.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+// import Root from 'root';
+
+document.addEventListener("DOMContentLoaded", () => {
+  const root = document.getElementById("root");
+
+  ReactDOM.render(<h1>LOUDCLOUD</h1>, root);
+})
