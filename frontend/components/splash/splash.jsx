d3afd51caa76f84c259dcf911a6e131a8ef42b4e
Tassosb <tassosbareiss@gmail.com>
2017-02-15 14:17:27 -0500

    Add buttons and logo to splash banner

--- frontend/components/splash/splash.jsx
+++ frontend/components/splash/splash.jsx
@@ -1,12 +1,24 @@
 import React from 'react';
+import LogInButton from '../buttons/log_in_button';
+import SignUpButton from '../buttons/sign_up_button';
+
+
+const SplashBanner = () => {
+  return (
+    <div className='splash-banner'>
+      <div className='nav-logo'>
+        <img src={ logoNoBackground }/>
+      </div>
+      <LogInButton />
+      <SignUpButton />
+    </div>
+  );
+}
 
 const Splash = () => {
   return (
     <div className='splash-main'>
-      <h1>Splash Page</h1>
-      <div className='splash-banner'>
-
-      </div>
+        <SplashBanner />
     </div>
   )
 }
