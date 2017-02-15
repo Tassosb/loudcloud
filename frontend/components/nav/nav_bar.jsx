76db270f672d7569902aae2a30598d53cd1608f9
Tassosb <tassosbareiss@gmail.com>
2017-02-15 13:52:18 -0500

    Add nav bar component

--- frontend/components/nav/nav_bar.jsx
+++ frontend/components/nav/nav_bar.jsx
@@ -2,6 +2,39 @@ import React from 'react';
 import { connect } from 'react-redux';
 import LogInButton from '../buttons/log_in_button';
 import SignUpButton from '../buttons/sign_up_button';
+import LogOutButton from '../buttons/log_out_button';
+
+const NavButtons = ({ loggedIn }) => (
+    <div className='nav-buttons'>
+      { !loggedIn &&
+        <div className='nav-buttons'>
+          <LogInButton />
+          <span>or</span>
+          <SignUpButton />
+        </div>
+      }
+      <div>Upload</div>
+    </div>
+)
+
+const NavDropDown = ({ loggedIn }) => {
+  const toggleDropDown = () => {
+    $('.nav-dropdown').toggleClass('display');
+  }
+
+  return (
+    <ul onClick={ toggleDropDown } className='nav-dropdown'>
+      <li >
+        <i className="fa fa-ellipsis-h drop-down-icon"
+          aria-hidden="true"></i>
+      </li>
+      <ul>
+        <li><span>Profile</span></li>
+        { loggedIn && <li><LogOutButton /></li>}
+      </ul>
+    </ul>
+  )
+}
 
 class NavBar extends React.Component {
   constructor (props) {
@@ -9,19 +42,26 @@ class NavBar extends React.Component {
   }
 
   render () {
+    const { currentUser } = this.props;
+
     return (
       <div className='nav-bar'>
         <div className='nav-logo'>
           <img src={ logoWithText } />
         </div>
-        <div className='nav-bar-buttons'>
-          <LogInButton />
-          <span>or</span>
-          <SignUpButton />
-        </div>
+        <NavButtons loggedIn={ !!currentUser } />
+        { !!currentUser &&
+          <div>{ currentUser.name || currentUser.email }</div> }
+        <NavDropDown loggedIn={ !!currentUser } />
       </div>
     )
   }
 }
 
-export default NavBar;
+const mapStateToProps = (state) => ({
+  currentUser: state.session.currentUser
+})
+
+export default connect(
+  mapStateToProps
+)(NavBar);
