6c020b1c33662e5243e95c8b2b067bf68ada7bd8
Tassosb <tassosbareiss@gmail.com>
2017-02-15 15:39:42 -0500

    Add demo user

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+User.destroy_all
+
+User.create!(email: 'demo@loudcloud.com', password: 'loudcloud', name: 'LoudCloud User')
