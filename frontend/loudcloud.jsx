790196d544da7d44876d8ac0e48e96395a099182
Tassosb <tassosbareiss@gmail.com>
2017-02-15 13:52:32 -0500

    Add more routes

--- frontend/loudcloud.jsx
+++ frontend/loudcloud.jsx
@@ -1,14 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Root from './root';
-import configureStore from './store/store';
 import { logIn, signUp, logOut } from './actions/session_actions';
 import { clearErrors } from './actions/errors_actions';
 
 document.addEventListener("DOMContentLoaded", () => {
   const root = document.getElementById("root");
-  const store = configureStore();
 
-  ReactDOM.render(<Root store={ store }/ >, root);
-  window.store = store;
+  ReactDOM.render(<Root />, root);
 })
--- frontend/root.jsx
+++ frontend/root.jsx
@@ -4,14 +4,31 @@ import { Router, hashHistory, IndexRoute, Route } from 'react-router';
 import App from './app';
 import LogInForm from './components/forms/log_in_form';
 import SignUpForm from './components/forms/sign_up_form';
+import configureStore from './store/store';
+import Splash from './components/splash/splash';
+import Stream from './components/browse/stream';
 
-const Root = ({ store }) => {
+
+const Root = () => {
+  const preloadedState = window.currentUser ?
+    { session: { currentUser: window.currentUser } } :
+    {};
+
+  delete window.currentUser;
+  const store = configureStore(preloadedState);
+  window.store = store;
+
+  const loggedIn = () => {
+    return !!store.getState().session.currentUser;
+  };
+
   return (
     <Provider store={ store }>
       <Router history={ hashHistory }>
         <Route path='/' component={ App }>
-          <Route path='/login' component={ LogInForm } />
-          <Route path='/signup' component={ SignUpForm } />
+          <IndexRoute component={ Splash } />
+          <Route path='/stream' component={ Stream }></Route>
+          <Route path='/profile' component={ Profile }></Route>
         </Route>
       </Router>
     </Provider>
