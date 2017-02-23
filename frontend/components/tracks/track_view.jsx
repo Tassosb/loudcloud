53b6b312949e0fb76b446644b926b5bd35e5aba1
Tassosb <tassosbareiss@gmail.com>
2017-02-23 09:52:11 -0500

    Add waveform to track show

--- frontend/components/tracks/track_view.jsx
+++ frontend/components/tracks/track_view.jsx
@@ -5,25 +5,32 @@ import PlayButton from '../buttons/play_button';
 import TrackStats from './track_stats';
 import CommentIndex from '../comments/comment_index';
 import CommentForm from '../comments/comment_form';
+import TrackWaveform from '../waveform/track_waveform';
 
 const TrackBanner = ({ track }) => {
 
   return (
     <div className='track-banner'>
-      <div className='track-header'>
-        { !!track.id &&
-          <PlayButton
-            trackId={ track.id }
-            trackQueuePos={ track.queuePos }
-            size='big' /> }
-        <div>
-          <Link to={`/users/${track.artist.id}`}>
-            { track.artist.name &&
+      <div className='track-banner-left'>
+        <div className='track-header'>
+          { !!track.id &&
+            <PlayButton
+              trackId={ track.id }
+              trackQueuePos={ track.queuePos }
+              size='big' /> }
+          <div>
+            <Link to={`/users/${track.artist.id}`}>
+              { track.artist.name &&
                 <div><h2>{ track.artist.name }</h2></div> }
-          </Link>
-          { track.title && <div><h1>{ track.title }</h1></div> }
+            </Link>
+            { track.title && <div><h1>{ track.title }</h1></div> }
+          </div>
+        </div>
+        <div className='waveform-small'>
+          <TrackWaveform track={ track } size='index' />
         </div>
       </div>
+
       <div className='track-image'>
         <img src={ track.image_url } />
       </div>
