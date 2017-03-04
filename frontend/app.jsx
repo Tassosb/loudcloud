41909b6d646a98324976ee959cc5cbe6a146e69c
Tassosb <tassosbareiss@gmail.com>
2017-03-04 12:59:14 -0500

    Add footer

--- frontend/app.jsx
+++ frontend/app.jsx
@@ -2,11 +2,14 @@ import React from 'react'
 import { connect } from 'react-redux';
 import Modal from './components/forms/modal';
 import AudioControlBar from './components/audio_player/audio_control_bar';
+import Footer from './components/footer/footer';
+
 const App = ({ children }) => {
   return (
     <div>
       <Modal />
       { children }
+      <Footer />
       <AudioControlBar />
     </div>
   )
---
+++ frontend/components/footer/footer.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <footer className='main-footer'>
+      <ul>
+        <li>
+          Made by Tassos Bareiss
+        </li>
+        <li>-</li>
+        <li>
+          <a href='https://github.com/Tassosb/loudcloud'>Github</a>
+        </li>
+        <li>-</li>
+        <li>
+          <a href='https://soundcloud.com'>Real SoundCloud</a>
+        </li>
+      </ul>
+    </footer>
+  )
+}
+
+export default Footer;
