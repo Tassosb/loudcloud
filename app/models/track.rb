f5a7762af3cf282b4459cec4a685b52a11e7a5e4
Tassosb <tassosbareiss@gmail.com>
2017-03-15 20:42:36 -0400

    Refactor liked_by? method

--- app/models/track.rb
+++ app/models/track.rb
@@ -67,10 +67,7 @@ class Track < ActiveRecord::Base
 
   def liked_by?(user)
     return false unless user
-    user.likes.to_a.find { |like| like.track_id == self.id }
-    # !!Like.joins(:track)
-    #       .where("user_id  = ? AND track_id = ?", user.id, self.id)
-    #       .first
+    user.likes.map(&:track_id).include?(self.id)
   end
 
   ###From EricMoy Songcloud
